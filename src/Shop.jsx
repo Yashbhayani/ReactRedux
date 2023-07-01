e68c71dd1e2fc034cc35ca87a4aca6630fa75a32
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2023-07-01 16:07:18 +0530

    Update Shop.jsx

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-// import { bindActionCreators } from "redux";
-import {actionCreators} from './State/index'
-
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./State/index";
 
 export const Shop = () => {
-  const dispath = useDispatch(); 
+  const dispath = useDispatch();
+  const action = bindActionCreators(actionCreators, dispath);
   return (
     <div className="container-fluid">
-        <h2>Deposit and Withdraw money</h2>
-
-      <button type="button" className="btn btn-primary mx-2" onClick={() => {dispath(actionCreators.WithdrawMoney(100))}}>
+      <h2>Deposit and Withdraw money</h2>
+      <button
+        type="button"
+        className="btn btn-primary mx-2"
+        onClick={() => {
+          action.WithdrawMoney(100);
+        }}
+      >
+        {/* <button type="button" className="btn btn-primary mx-2" onClick={() => {dispath(actionCreators.WithdrawMoney(100))}}> */}
         -
       </button>
       Add to cart
-      <button type="button" className="btn btn-warning mx-2" onClick={() => {dispath(actionCreators.depositMoney(100))}}>
+      {/* <button
+        type="button"
+        className="btn btn-warning mx-2"
+        onClick={() => {
+          dispath(actionCreators.depositMoney(100));
+        }}
+      > */}
+      <button
+        type="button"
+        className="btn btn-primary mx-2"
+        onClick={() => {
+          action.depositMoney(100);
+        }}
+      >
         +
       </button>
     </div>
   );
 };
- 
